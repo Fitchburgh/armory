aec2bc38548e91abb3f9e49c32f95039b5ebc58d
Brandon Fitch <fitchbj@gmail.com>
2016-10-10 16:32:20 -0400

    Minor syntax error

--- db/migrate/003_create_characters.rb
+++ db/migrate/003_create_characters.rb
@@ -2,7 +2,7 @@ require 'active_record'
 #
 class CreateCharacters < ActiveRecord::Migration[5.0]
   def up
-    create_join_table :armors, :weapons, :characters do |t|
+    create_join_table :armors, :weapons, table_name: :characters do |t|
       t.primary_key
       t.references :armor, index: true, foreign_key: true
       t.references :weapon, index: true, foreign_key: true

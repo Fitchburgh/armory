50d84d354e31f839302363b6410ec7707ba7ac91
Brandon Fitch <fitchbj@gmail.com>
2016-10-10 19:26:45 -0400

    slot and piece name post working

--- public/js/main.js
+++ public/js/main.js
@@ -19,20 +19,82 @@
 
 var $characterName = $('#character-name');
 
-function anything() {
+function getCharName() {
   $.ajax({
     method: 'GET',
     url: '/api/character_list',
-    date: {},
+    data: {},
     "datatype": "json",
   }).done(function(character) {
     new CharacterDetails(character[1]); // event listener
   }).fail(function(xhr, text, status) {
     if (xhr.status == 404)
-    console.log('help computer!', text, status, xhr.status);
+    console.log('help me, computer!', text, status, xhr.status);
   })
 }
-anything();
+getCharName();
+
+// $('#armorSlotInput').keypress(function(event) {
+//     var inputStringOne = $('#armorSlotInput').val();
+//     if (event.which == 13) {
+//         newArmorPiece(inputStringOne);
+//         return false;
+//     }
+// });
+//
+// $('#armorPieceName').keypress(function(event) {
+//     var inputStringTwo = $('#armorPieceName').val();
+//     if (event.which == 13) {
+//         newArmorPiece(inputStringTwo);
+//         return false;
+//     }
+// });
+//
+// function newArmorPiece(inputStringOne, inputStringTwo) {
+//   var armorAdd = $("#addArmorBtn").val("");
+//   $.ajax({
+//     method: 'POST',
+//     url: '/api/add_armor_piece',
+//     data: {
+//       armor_slot: inputStringOne,
+//       armor_name: inputStringTwo,
+//     },
+//     datatype: 'json',
+//   })
+// };
+
+var $armorSlot = $('#armorSlotInput')
+var $armorPiece = $('#armorPieceName')
+
+$("#addArmorBtn").on('click', function newArmorPiece() {
+  var slot = $armorSlot.val();
+  var piece = $armorPiece.val();
+
+  $('#armorSlotInput').val('');
+
+  $.ajax({
+    method: 'POST',
+    url: '/api/add_armor_piece',
+    datatype: 'json',
+    data: {
+      armor_slot: slot,
+      armor_name: piece
+    }
+  })
+})
+
+//
+//   }).done(function(armor) {
+//     new ArmorDetails(armor);
+//
+//   }).fail(function(xhr, text, status) {
+//     if (xhr.status == 404)
+//     console.log('help me, computer!', text, status, xhr.status);
+//   })
+//   console.log(armorAdd)
+// }
+
+
 
 function CharacterDetails(characterObject) {
   console.log(characterObject);
@@ -45,6 +107,8 @@ function CharacterDetails(characterObject) {
     var charContainer = $("<div>").attr("class", this.info.charName);
     var charName = $("<p>").attr("id", this.info.charName).html(this.info.charName).appendTo(charContainer);
     $(charContainer).insertAfter("header");
+
+    // $("#" + this.info.charname)
 
     this.MagicElements = function(characterObject) {
         var context = {
